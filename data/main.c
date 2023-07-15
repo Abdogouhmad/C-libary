fb6c3167ac53d007dd60472898934bb92425965b
div-styl <gouhmada@gmail.com>
2023-07-15 11:03:23 +0100

    I created a do while loop for that

--- data/main.c
+++ data/main.c
@@ -19,36 +19,27 @@ int main(void)
 
     srand(time(NULL));
     key = rand();
-
-    printf("Enter the value as a string: ");
-
-    buffer_size = 1024;
-    value = malloc(sizeof(char) * buffer_size);
-    if (value == NULL)
+    
+    do
     {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(2);
-    }
-
-    input_len = getline(&value, &buffer_size, stdin);
-    if (input_len == -1)
-    {
-        fprintf(stderr, "Failed to read input\n");
-        exit(2);
-    }
-
-    /* Remove newline character if present */
-    value_len = strlen(value);
-    if (value_len > 0 && value[value_len - 1] == '\n')
-        value[value_len - 1] = '\0';
-
-    fprintf(data, "- %d: \t\t %s \n", key, value);
+        printf("Enter the value as a string (enter 'exit' to exit): ");
+        buffer_size = 0;  /*Reset buffer size before getline*/
+        input_len = getline(&value, &buffer_size, stdin);
+        if (input_len == -1)
+        {
+            fprintf(stderr, "Failed to read input\n");
+            exit(2);
+        }
+        value_len = strlen(value);
+        if (value_len > 0 && value[value_len - 1] == '\n')
+            value[value_len - 1] = '\0';
+            
+        fprintf(data, "ID: %d:\t\t%s\n", key, value);
+    } while (strcmp(value, "exit") != 0);
 
     fclose(data);
     free(value);
 
     return 0;
 }
-
-
 
